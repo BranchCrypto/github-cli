11c0130d3ad41233e14e498f16e73ae830726b8a
BranchCrypto <BranchCrypto@users.noreply.github.com>
2026-03-19 14:16:39 +0800

    Remove temp files

--- _push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\Administrator\Desktop\github
-git add -A
-git commit --allow-empty -m "Remove temp files"
-set HTTPS_PROXY=http://127.0.0.1:7890
-set HTTP_PROXY=http://127.0.0.1:7890
-git push
-echo === DONE ===
