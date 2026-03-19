9731453f6628f3a6417da28b4c4c760f71dc43a3
BranchCrypto <BranchCrypto@users.noreply.github.com>
2026-03-19 14:15:53 +0800

    Final cleanup

--- _final.bat
+++ _final.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d C:\Users\Administrator\Desktop\github
-del _clean.bat
+del _clean2.bat
 git add -A
-git commit -m "Clean up"
+git commit --allow-empty -m "Final cleanup"
 set HTTPS_PROXY=http://127.0.0.1:7890
 set HTTP_PROXY=http://127.0.0.1:7890
 git push
